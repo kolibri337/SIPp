c4a48b462f694c7dea384f635030528e34645cfa
Bondarenko Nikita <bondar3nko.nikit@yandex.ru>
2025-02-15 22:19:01 +0300

    Added sed for placeholders in xml scenario file

--- run_calls.sh
+++ run_calls.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
 # Проверка обязательных переменных окружения
-if [ -z "$REMOTE_IP" ]; then
-  echo "Ошибка: переменная окружения REMOTE_IP не задана."
-  exit 1
-fi
+for var in REMOTE_IP LOCAL_IP; do
+  if [ -z "${!var}" ]; then
+    echo "Ошибка: переменная окружения $var не задана."
+    exit 1
+  fi
+done
 
-if [ -z "$LOCAL_IP" ]; then
-  echo "Ошибка: переменная окружения LOCAL_IP не задана."
-  exit 1
-fi
-
-# Параметры запуска sipP
+# Параметры запуска SIPp
 REMOTE_PORT=${REMOTE_PORT:-"5060"}     # Порт на удалённом сервере
 LOCAL_PORT=${LOCAL_PORT:-"5060"}       # Порт на клиенте
 CALL_COUNT=${CALL_COUNT:-30}           # Количество вызовов
 CALL_INTERVAL=${CALL_INTERVAL:-30}     # Интервал между вызовами (секунды)
-# Минимальная и максимальная длительность вызова (минуты)
-MIN_DURATION=${MIN_DURATION:-2}  # 2 минуты по умолчанию
-MAX_DURATION=${MAX_DURATION:-6}  # 6 минут по умолчанию
+MIN_DURATION=${MIN_DURATION:-2}        # Минимальная длительность вызова (минуты)
+MAX_DURATION=${MAX_DURATION:-6}        # Максимальная длительность вызова (минуты)
 
 # Преобразуем минуты в миллисекунды
 MIN_DURATION_MS=$((MIN_DURATION * 60 * 1000))
@@ -27,13 +23,22 @@ MAX_DURATION_MS=$((MAX_DURATION * 60 * 1000))
 # Диапазон времени для генерирования продолжительности звонков
 RANGE=$((MAX_DURATION_MS - MIN_DURATION_MS))
 
+# Заменяем плейсхолдеры в scenario.xml
+sed -i "s/\[REMOTE_IP\]/$REMOTE_IP/g" /app/scenario.xml
+sed -i "s/\[REMOTE_PORT\]/$REMOTE_PORT/g" /app/scenario.xml
+sed -i "s/\[LOCAL_IP\]/$LOCAL_IP/g" /app/scenario.xml
+sed -i "s/\[LOCAL_PORT\]/$LOCAL_PORT/g" /app/scenario.xml
+
 for ((i=1; i<=CALL_COUNT; i++))
 do
   # Генерация случайной длительности вызова
   CALL_DURATION=$(( (RANDOM % RANGE) + MIN_DURATION_MS ))
 
+  # Заменяем плейсхолдер [call_duration] в scenario.xml
+  sed -i "s/\[call_duration\]/$CALL_DURATION/g" /app/scenario.xml
+
   # Запускаем SIPp с указанными параметрами
-  sipp -sf scenario.xml -i $LOCAL_IP -p $LOCAL_PORT -m 1 -d $CALL_DURATION $REMOTE_IP:$REMOTE_PORT
+  sipp -sf /app/scenario.xml -i $LOCAL_IP -p $LOCAL_PORT -m 1 -d $CALL_DURATION $REMOTE_IP:$REMOTE_PORT
 
   # Пауза между вызовами
   if [ $i -lt $CALL_COUNT ]; then
