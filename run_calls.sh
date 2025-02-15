eca2b2fa45f102f3b6f4487bb4fcc8200290d8fa
Bondarenko Nikita <bondar3nko.nikit@yandex.ru>
2025-02-15 22:23:04 +0300

    Added scenario_template file for 'for' iterations

--- run_calls.sh
+++ run_calls.sh
@@ -23,25 +23,32 @@ MAX_DURATION_MS=$((MAX_DURATION * 60 * 1000))
 # Диапазон времени для генерирования продолжительности звонков
 RANGE=$((MAX_DURATION_MS - MIN_DURATION_MS))
 
-# Заменяем плейсхолдеры в scenario.xml
-sed -i "s/\[REMOTE_IP\]/$REMOTE_IP/g" /app/scenario.xml
-sed -i "s/\[REMOTE_PORT\]/$REMOTE_PORT/g" /app/scenario.xml
-sed -i "s/\[LOCAL_IP\]/$LOCAL_IP/g" /app/scenario.xml
-sed -i "s/\[LOCAL_PORT\]/$LOCAL_PORT/g" /app/scenario.xml
+# Создаем копию оригинального scenario.xml
+cp scenario.xml scenario_template.xml
 
 for ((i=1; i<=CALL_COUNT; i++))
 do
   # Генерация случайной длительности вызова
   CALL_DURATION=$(( (RANDOM % RANGE) + MIN_DURATION_MS ))
 
-  # Заменяем плейсхолдер [call_duration] в scenario.xml
-  sed -i "s/\[call_duration\]/$CALL_DURATION/g" /app/scenario.xml
+  # Восстанавливаем оригинальный scenario.xml из копии
+  cp scenario_template.xml scenario.xml
+
+  # Заменяем плейсхолдеры в scenario.xml
+  sed -i "s/\[REMOTE_IP\]/$REMOTE_IP/g" scenario.xml
+  sed -i "s/\[REMOTE_PORT\]/$REMOTE_PORT/g" scenario.xml
+  sed -i "s/\[LOCAL_IP\]/$LOCAL_IP/g" scenario.xml
+  sed -i "s/\[LOCAL_PORT\]/$LOCAL_PORT/g" scenario.xml
+  sed -i "s/\[call_duration\]/$CALL_DURATION/g" scenario.xml
 
   # Запускаем SIPp с указанными параметрами
-  sipp -sf /app/scenario.xml -i $LOCAL_IP -p $LOCAL_PORT -m 1 -d $CALL_DURATION $REMOTE_IP:$REMOTE_PORT
+  sipp -sf scenario.xml -i $LOCAL_IP -p $LOCAL_PORT -m 1 -d $CALL_DURATION $REMOTE_IP:$REMOTE_PORT
 
   # Пауза между вызовами
   if [ $i -lt $CALL_COUNT ]; then
     sleep $CALL_INTERVAL
   fi
 done
+
+# Удаляем временный файл
+rm scenario_template.xml
